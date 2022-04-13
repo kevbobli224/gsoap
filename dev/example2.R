26707d875fbf7c3faf4daf663aca4b876d755a1c
likevi23 <kevin224li@gmail.com>
2022-04-13 11:04:10 -0400

    Updated example file

--- dev/example2.R
+++ dev/example2.R
@@ -4,7 +4,7 @@
 library(gsoap)
 library(ggforce)
 library(ggrepel)
-library("ggiraph")
+library(ggiraph)
 library(htmlwidgets)
 
 # Load example dataset
@@ -21,11 +21,17 @@ layout <-  layout[order(layout$significance, decreasing = TRUE),]
 rownames(layout) <- seq(1:nrow(layout))
 
 # Create svg with layout as parameters
-p <- gsoap_svg(layout, as.color = 'cluster', as.alpha = 'significance', which.labels = c("a6b1 and a6b4 Integrin signaling","Cardiac_Hypertrophy"))
-p <- gsoap_svg(layout, as.color = 'cluster', as.alpha = 'significance', which.labels = 1:10)
+p <- gsoap_svg(layout, as.color = 'cluster', as.alpha = 'significance')
+
+# If needed to generate labels on svg:
+#p <- gsoap_svg(layout, as.color = 'cluster', as.alpha = 'significance', which.labels = 1:10)
 
 # Produce interactive html svg
-w <- ggiraph(ggobj = p, width_svg = 7, height_svg = 7)
+w <- ggiraph(ggobj = p)
+w <- girafe_options(w, opts_toolbar(position = "topleft"), opts_zoom(min = 0.2, max = 5))
+
+# To view in RStudio's viewer:
+w
 
 # Create an output directory, if exists warning will be hidden
 dir.create(file.path(getwd(), "output"), showWarnings = FALSE)
@@ -39,8 +45,6 @@ unlink(file.path(getwd(), "output/widgets_files"), recursive = TRUE)
 
 
 
-# To view in RStudio's viewer:
-w
 
 # To save as pure svg, uncomment
 # dsvg(file="./output/widgets.svg")
@@ -51,6 +55,7 @@ w
 library("xml2")
 h <- read_html("./output/widgets.html")
 
+# Attaches Jquary dependencies
 jQ <- read_xml('<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>')
 jQUI <- read_xml('<script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>')
 jQss <- read_xml('<link href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css" type="text/css" rel="stylesheet"></link>')
@@ -59,9 +64,12 @@ xml_add_child(xml_children(h)[1], jQ)
 xml_add_child(xml_children(h)[1], jQUI)
 xml_add_child(xml_children(h)[1], jQss)
 
+# Attaches our own interactive js script
 tag <- paste0("<script></script>")
 post_proc <- read_xml(tag)
 xml_text(post_proc) <- as.character(readLines("./output/postproc_min.js"))
 
 xml_add_child(xml_children(h)[1], post_proc)
+
+# Write to file
 write_html(h, "./output/widgets_modified.html")
